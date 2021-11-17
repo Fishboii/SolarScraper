b8c64f2238565cfacd0c731e2f8eaff13c561b11
Jason Zhang <45155521+Fishboii@users.noreply.github.com>
2021-11-17 02:11:58 +0000

    Completed add data to csv

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ from math import sqrt
 #find % diff from minimum colour
 def clrdiff(minclr, maxclr, clr):
     #only takes tupules/list (R, G, B)
-    l = sqrt((maxclr[0]-minclr[0])^2+(maxclr[1]-minclr[1])^2+(maxclr[2]-minclr[2])^2)
-    diffclr = sqrt((clr[0]-minclr[0])^2+(clr[1]-minclr[1])^2+(clr[2]-minclr[2])^2)
-    1+1
-    return diffclr/l
-    1+1
+    l = sqrt((maxclr[0]-minclr[0])**2+(maxclr[1]-minclr[1])**2+(maxclr[2]-minclr[2])**2)
+    diffclr = sqrt((clr[0]-minclr[0])**2+(clr[1]-minclr[1])**2+(clr[2]-minclr[2])**2)
+    return (diffclr/l)
 
 #process HTML from requests using bs4 parser
 html_page = requests.get('https://www.ema.gov.sg/solarmap.aspx')
@@ -32,20 +30,28 @@ im = Image.open(filename)
 
 #set of points in image to get colours of
 clr = []
-#points = [(1389,1120), (1313,1141), (1212,1082), (1179,1149), (990,1049), (1338,1036), (1389,1015), (1368,952), (1292,986), (1128,948), (1208,877), (1397,868), (1502,826), (1590,919), (1569,1011), (1871,872), (2039,726), (1783,684), (1527,654), (1191,742), (977,885), (570,847), (881,747), (583,583), (906,499), (1107,591), (1162,310), (1405,541), (444,1233), (1254,1267), (58,54), (2442,163)]
-points = [(58,54)]
+points = [(1389,1120), (1313,1141), (1212,1082), (1179,1149), (990,1049), (1338,1036), (1389,1015), (1368,952), (1292,986), (1128,948), (1208,877), (1397,868), (1502,826), (1590,919), (1569,1011), (1871,872), (2039,726), (1783,684), (1527,654), (1191,742), (977,885), (570,847), (881,747), (583,583), (906,499), (1107,591), (1162,310), (1405,541), (444,1233), (1254,1267), (58,54), (2442,163), (2446,1384)]
+
 for i in points:
     clr.append(im.getpixel((i[0], i[1])))
-
-print(clr)
 
 
 #convert colour of each pixel to %
 pctgclr = []
 for i in clr:
-    pctgclr.append(clrdiff((0,100,0), (139, 241, 117), i))
+    pctgclr.append(clrdiff((0,100,0), (141, 239, 124), i))
 
-print(pctgclr)
+#check control variables
+if (pctgclr[-2],pctgclr[-1]) != (1.0, 0.0):
+    raise NameError("Minmax values does not match minmax points, min = {}, max = {}".format(pctgclr[-1], pctgclr[-2]))
+
+#plug numbers into csv file
+fileappend = open("data.csv", "a")
+fileappend.write("\n{}".format(time.strftime("%y, %m, %d, %H, %M", time.gmtime()).replace(", ","")))
+for i in range(len(pctgclr)-3):
+    fileappend.write(", {}".format(pctgclr[i]))
     
+fileappend.close()
+print("complete")
 
-#write data to excel file
+
